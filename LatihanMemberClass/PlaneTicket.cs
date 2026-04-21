e130756857098a9f712d378b3c25b0de4bb8a114
fradyv <fastunadya@gmail.com>
2026-04-21 14:58:19 +0700

    Add files via upload

---
+++ LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+        private string _origin;
+        private string _destination;
+        private float _cost;
+        private string _currency;
+
+        public string Origin
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+        public string Destination
+        { 
+            get { return _destination; }
+            set { _destination = value; }
+        }
+        public float Cost
+        { 
+            get { return _cost; }
+            set { _cost = value; }
+        }
+        public string Currency
+        {
+            get { return _currency; }
+            set { _currency = value; }
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destination: {0}", Destination);
+            Console.WriteLine("Cost: {0}", Cost);
+            Console.WriteLine("Convert to: {0}", Currency);
+        }
+    }
+    
+}
---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket tiket1 = new PlaneTicket();
+            PlaneTicket tiket2 = new PlaneTicket();
+
+            tiket1.Origin = "JOGJA";
+            tiket2.Origin = "JKT";
+            tiket1.Destination = "PKU";
+            tiket2.Destination = "PKU";
+            tiket1.Cost = 100;
+            tiket2.Cost = 750000;
+            tiket1.Currency = "USD";
+            tiket2.Currency = "IDR";
+
+            Console.WriteLine("Origin: {0}", tiket1.Origin);
+            Console.WriteLine("Destination: {0}", tiket1.Destination);
+
+        }
+    }
+}
